d843dc9cf2adfb0f31044c7d3dcddd66a37499e6
Liz <lee.anewed@gmail.com>
2021-02-23 08:46:49 -0600

    Added NPC and NPC movement

---
+++ Guardians of the Forest/Assets/Scripts/VillagerMovement.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    private Rigidbody2D myRigidBody;
+
+    public bool isWalking;
+
+    public float walktime;
+    private float walkCounter;
+    public float waitTime;
+    private float waitCounter;
+
+    private int walkDirection;
+
+    public Collider2D walkZone;
+    private Vector2 minWalkPoint;
+    private Vector2 maxWalkPoint;
+    private bool hasWalkZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        waitCounter = waitTime;
+        walkCounter = walktime;
+
+        ChooseDirection();
+        if(walkZone != null)
+        {
+            //value of bottom left corner of Collider
+            minWalkPoint = walkZone.bounds.min;
+            //value of top right corner of Collider
+            maxWalkPoint = walkZone.bounds.max;
+
+            hasWalkZone = true;
+        }    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWalking)
+        {
+            walkCounter -= Time.deltaTime;
+
+            switch (walkDirection)
+            {
+                case 0:
+                    //move up
+                    myRigidBody.velocity = new Vector2(0, moveSpeed);
+                    if(hasWalkZone && transform.position.y > maxWalkPoint.y)
+                    {
+                        isWalking = false;
+                        waitCounter = waitTime;
+                    }
+                    break;
+                case 1:
+                    //move right
+                    myRigidBody.velocity = new Vector2(moveSpeed, 0);
+                    if (hasWalkZone && transform.position.x > maxWalkPoint.x)
+                    {
+                        isWalking = false;
+                        waitCounter = waitTime;
+                    }
+                    break;
+                case 2:
+                    //move down
+                    myRigidBody.velocity = new Vector2(0, -moveSpeed);
+                    if (hasWalkZone && transform.position.y < minWalkPoint.y)
+                    {
+                        isWalking = false;
+                        waitCounter = waitTime;
+                    }
+                    break;
+                case 4:
+                    //move left
+                    myRigidBody.velocity = new Vector2(-moveSpeed, 0);
+                    if (hasWalkZone && transform.position.x < minWalkPoint.x)
+                    {
+                        isWalking = false;
+                        waitCounter = waitTime;
+                    }
+                    break;
+            }
+            if (walkCounter < 0)
+            {
+                isWalking = false;
+                waitCounter = waitTime;
+            }
+
+        } else
+        {
+            waitCounter -= Time.deltaTime;
+
+            myRigidBody.velocity = Vector2.zero;
+
+            if (waitCounter < 0)
+            {
+                ChooseDirection();
+            }
+        }
+    }
+
+    public void ChooseDirection()
+    {
+        //because we are using int (and not float), Random.Range will not pick the top number so this will only return 0, 1, 2, or 3
+        walkDirection = Random.Range(0, 4);
+        isWalking = true;
+        walkCounter = walktime;
+    }
+}
